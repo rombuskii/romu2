369cb8f0983fd82038a1fba4bd5575ed757aa6be
erubae <rombuski@gmail.com>
2023-07-10 12:33:53 +0100

    Update index.js

--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -7,6 +7,7 @@ import { useAuth } from '@/context/AuthContext'
 const Settings = () => {
     const {currentUser} = useAuth();
     const [imageUpload, setImageUpload] = useState(null)
+    const [msg, setMsg] = useState('')
     console.log(currentUser)
 
     const upload = (e) => {
@@ -16,7 +17,8 @@ const Settings = () => {
         uploadBytes(imageRef, imageUpload).then((snapshot) => {
             getDownloadURL(snapshot.ref).then((url) => {
                 updateProfile(currentUser, {photoURL: url })
-        })
+                setMsg("Uploaded Successfully")
+        }).catch(error => setMsg('Something went wrong'))
     })
     }
 
@@ -26,7 +28,7 @@ const Settings = () => {
         <div><h1 className='text-xl sm:text-5xl'>Settings</h1></div>
         <div className='w-full'>
             <form>
-                <p>Upload Profile Pic:</p>
+                <p>Upload Profile Pic: {msg}</p>
                 <input className='w-full max-w-[40ch] hover:opaciy-40 cursor-pointer my-5 border-2 ' onChange={(e) => setImageUpload(e.target.files[0])} type="file" name="filename"/>
                 <div><button onClick={upload} type='submit' className='duration-300 hover:text-blue-500 cursor-pointer hover:border-blue-500 bg-white rounded-lg p-1 text-black'>Upload Image</button></div>
             </form>
