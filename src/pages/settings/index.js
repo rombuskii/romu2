009139c77e085b4c45480826341403c87202843b
erubae <rombuski@gmail.com>
2023-07-10 12:30:00 +0100

    Fixed profile picture bug

--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -7,38 +7,28 @@ import { useAuth } from '@/context/AuthContext'
 const Settings = () => {
     const {currentUser} = useAuth();
     const [imageUpload, setImageUpload] = useState(null)
+    console.log(currentUser)
 
     const upload = (e) => {
-        let imgUrl;
+        e.preventDefault()
         if(imageUpload == null) return;
-        const imgPath = ref(storage, "images/")
-        const imageRef = ref(storage, `images/pfp`)
-        uploadBytes(imageRef, imageUpload).then(() => {
-            alert('Image Uploaded')
-            listAll(imgPath).then((response) => {
-                response.items.forEach((item) => {
-                    getDownloadURL(item).then((url) => {
-                        imgUrl = [url]
-                        updateProfile(currentUser, {photoURL: imgUrl[0] })
-                        .then(() => {
-                            return;
-                        })
-                        .catch(error => null) 
-                    })
-                }
-                )
-            })
+        const imageRef = ref(storage, `images/${currentUser.uid}`)
+        uploadBytes(imageRef, imageUpload).then((snapshot) => {
+            getDownloadURL(snapshot.ref).then((url) => {
+                updateProfile(currentUser, {photoURL: url })
         })
+    })
     }
+
   return (
     
     <div className='flex gap-5 flex-col justify-evenly items-center'>
         <div><h1 className='text-xl sm:text-5xl'>Settings</h1></div>
         <div className='w-full'>
-            <form onSubmit={upload}>
+            <form>
                 <p>Upload Profile Pic:</p>
-                <input id='pfp' className='w-full max-w-[40ch] hover:opaciy-40 cursor-pointer my-5 border-2 ' onChange={(e) => setImageUpload(e.target.files[0])} type="file" name="filename"/>
-                <div><button type='submit' className='duration-300 hover:text-blue-500 cursor-pointer hover:border-blue-500 bg-white rounded-lg p-1 text-black'>Upload Image</button></div>
+                <input className='w-full max-w-[40ch] hover:opaciy-40 cursor-pointer my-5 border-2 ' onChange={(e) => setImageUpload(e.target.files[0])} type="file" name="filename"/>
+                <div><button onClick={upload} type='submit' className='duration-300 hover:text-blue-500 cursor-pointer hover:border-blue-500 bg-white rounded-lg p-1 text-black'>Upload Image</button></div>
             </form>
         </div>
     </div>
