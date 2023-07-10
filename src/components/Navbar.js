616696ec35f0c5e16608f9be17071981667aa1a8
erubae <rombuski@gmail.com>
2023-07-10 08:07:01 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ const Navbar = () => {
     <div className='z-10 sticky top-0 left-0 w-full bg-slate-900 border-b border-solid border-white text-white flex items-center gap-x-2 justify-between p-4'>
         <h1 onClick={() => router.push('/')} className='duration-300 hover:-translate-y-1 cursor-pointer hover:scale-110 text-3xl sm:text-6xl'>ROMU</h1>
         {currentUser && <SearchBar search={search} setSearch={setSearch} handleSearch={handleSearch}/>}
-        {currentUser.photoURL? <div className=''><Image onClick={() => setOpenModal(true)} src={currentUser.photoURL}  width={100} height={100} className='rounded-3xl duration-300 hover:scale-110 cursor-pointer'/></div> : <i onClick={() => setOpenModal(true)} className="fa-solid fa-user text-xl sm:text-3xl duration-300 hover:opacity-40 cursor-pointer"></i>}
+        {currentUser && currentUser?.photoURL ? <div className=''><Image onClick={() => setOpenModal(true)} src={currentUser.photoURL}  width={100} height={100} className='rounded-3xl duration-300 hover:scale-110 cursor-pointer'/></div> : <i onClick={() => setOpenModal(true)} className="fa-solid fa-user text-xl sm:text-3xl duration-300 hover:opacity-40 cursor-pointer"></i>}
     </div>
     </>
   )
